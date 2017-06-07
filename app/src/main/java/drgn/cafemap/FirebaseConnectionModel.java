54390825310bf1bfc6941df1b147f97dbaa757c4
Musta <musta@XGP-M.local>
2017-06-07 08:54:03 -0700

    Change name

--- app/src/main/java/drgn/cafemap/FirebaseConnectionModel.java
+++ app/src/main/java/drgn/cafemap/FirebaseConnectionModel.java
@@ -5,10 +5,12 @@ import android.util.Log;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import static android.content.ContentValues.TAG;
@@ -21,6 +23,7 @@ public class FirebaseConnectionModel {
 
     private DatabaseReference mDatabase;
     private GoogleMap mMap;
+    private String testValue = "null";
 
     public FirebaseConnectionModel(GoogleMap mMap) {
 
@@ -32,6 +35,55 @@ public class FirebaseConnectionModel {
     }
 
     protected void setMarkersOnGoogleMap() {
+        Query query = mDatabase.child("MarkerList").orderByChild("name").equalTo("HOME");
+
+//        query.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                System.out.println("Firebase ------ onChildAdded " +dataSnapshot.getValue());
+//                String name;
+//                MarkerOptions options;
+//                LatLng location;
+//
+//                // location start from 1
+//                for (int i = 1; ; i++) {
+//                    if ((name = dataSnapshot.child("location" + String.valueOf(i)).child("name").getValue(String.class)) == null)
+//                        break;
+//
+//                    options = new MarkerOptions();
+//                    options.title(name);
+//                    location = new LatLng(dataSnapshot.child("location" + String.valueOf(i)).child("lat").getValue(Double.class),
+//                            dataSnapshot.child("location" + String.valueOf(i)).child("lon").getValue(Double.class));
+//                    options.position(location);
+//                    options.snippet("Wi-fi: " + dataSnapshot.child("location" + String.valueOf(i)).child("wifi").getValue(String.class));
+//
+//                    // マップにマーカー追加
+//                    mMap.addMarker(options);
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//                System.out.println("Firebase ------ onChildChanged");
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
 
         mDatabase.child("MarkerList").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -59,6 +111,8 @@ public class FirebaseConnectionModel {
                     mMap.addMarker(options);
 
                 }
+
+                testValue = "INITIALIZE----";
             }
 
             @Override
@@ -68,5 +122,10 @@ public class FirebaseConnectionModel {
             }
         });
 
+    }
+
+    public String getTestValue() {
+        System.out.println(testValue+" ==============");
+        return testValue;
     }
 }
