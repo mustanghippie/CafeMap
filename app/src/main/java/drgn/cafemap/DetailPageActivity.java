121351fe0ed027c180433e831a93b735697752e0
GeorgeRyu <r.takahasii@gmail.com>
2017-06-08 00:02:56 -0700

    Ryu commit 2

--- app/src/main/java/drgn/cafemap/DetailPageActivity.java
+++ app/src/main/java/drgn/cafemap/DetailPageActivity.java
@@ -7,16 +7,25 @@ import android.widget.TextView;
 public class DetailPageActivity extends AppCompatActivity {
 
     private TextView nameTextView;
+    private TextView timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
 
-        System.out.println("Ryu¥¥¥¥¥");
-
         nameTextView = (TextView) findViewById(R.id.cafeName);
         nameTextView.setText(getIntent().getStringExtra("key"));
+
+        timeTextView = (TextView) findViewById(R.id.cafeTime);
+
+        timeTextView = (TextView) findViewById(R.id.cafeTel);
+
+        timeTextView = (TextView) findViewById(R.id.cafeSocket);
+
+        timeTextView = (TextView) findViewById(R.id.cafeWifi);
+
+        timeTextView = (TextView) findViewById(R.id.cafePower);
 
         MapsActivity.fcm.getTestValue();
 
