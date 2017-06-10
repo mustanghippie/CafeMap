ecf6cd961fd5e5773d90821cde97d5b0eb8a5968
GeorgeRyu <r.takahasii@gmail.com>
2017-06-09 21:50:16 -0700

    modify the main photo size on detail page

--- app/src/main/java/drgn/cafemap/DetailPageActivity.java
+++ app/src/main/java/drgn/cafemap/DetailPageActivity.java
@@ -1,10 +1,17 @@
 package drgn.cafemap;
 
+import android.app.ActionBar;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.gms.maps.model.Marker;
 
 import java.util.HashMap;
+
+import static drgn.cafemap.MapsActivity.atms;
 
 public class DetailPageActivity extends AppCompatActivity {
 
@@ -23,7 +30,16 @@ public class DetailPageActivity extends AppCompatActivity {
         // check a new marker
         String key = getIntent().getStringExtra("indexKey"); // latitude + longitude
 
-        HashMap<String, String> cafeDetail = MapsActivity.atms.getCafeMap().get(key);
+        HashMap<String, String> cafeDetail = atms.getCafeMap().get(key);
+
+
+        View view = getLayoutInflater().inflate(R.layout.activity_detail_page, null);
+
+//        ImageView img = (ImageView) view.findViewById(R.id.badge);
+//        Bitmap image = atms.getCafeBitmapMap().get(marker.getTitle().replaceAll(" ", "_").toLowerCase());
+//        img.setImageBitmap(image);
+//        atms.getCafeBitmap().get("trees_organic_coffee");
+
 
         nameTextView = (TextView) findViewById(R.id.cafeName);
         nameTextView.setText(cafeDetail.get("name"));
