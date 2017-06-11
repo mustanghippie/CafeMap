c21233f7c521892d197c088a85cdadd6afcaddae
Musta <musta@XGP-M.local>
2017-06-11 08:10:14 -0700

    Add user cafe map function

--- app/src/main/java/drgn/cafemap/DetailPageActivity.java
+++ app/src/main/java/drgn/cafemap/DetailPageActivity.java
@@ -3,10 +3,12 @@ package drgn.cafemap;
 import android.app.ActionBar;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.HashMap;
@@ -15,42 +17,25 @@ import static drgn.cafemap.MapsActivity.atms;
 
 public class DetailPageActivity extends AppCompatActivity {
 
-    private TextView nameTextView;
-    private TextView addressTextView;
-    private TextView timeTextView;
-    private TextView telTextView;
-    private TextView socketTextView;
-    private TextView wifiTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_page);
 
-        String key = getIntent().getStringExtra("indexKey"); // latitude + longitude
-
-        HashMap<String, String> cafeDetail = atms.getCafeMap().get(key);
-        String imageName = MapsActivity.atms.getCafeMap().get(key).get("name").replaceAll(" ", "_").toLowerCase();
-        Bitmap image = atms.getCafeBitmapMap().get(imageName);
-
-        // Set image
-        ImageView img = (ImageView) findViewById(R.id.badge);
-        img.setImageBitmap(image);
-
-
-        nameTextView = (TextView) findViewById(R.id.cafeName);
-        nameTextView.setText(cafeDetail.get("name"));
-        addressTextView = (TextView) findViewById(R.id.cafeAddress);
-        addressTextView.setText(cafeDetail.get("address"));
-        telTextView = (TextView) findViewById(R.id.cafeTel);
-        telTextView.setText(cafeDetail.get("tel"));
-        timeTextView = (TextView) findViewById(R.id.cafeTime);
-        timeTextView.setText(cafeDetail.get("time"));
-        socketTextView = (TextView) findViewById(R.id.cafeSocket);
-        socketTextView.setText("(Socket) " + cafeDetail.get("socket"));
-        wifiTextView = (TextView) findViewById(R.id.cafeWifi);
-        wifiTextView.setText("(Wi-fi) " + cafeDetail.get("wifi"));
-
+        // make fragment
+        FragmentDetailPage fragment = new FragmentDetailPage();
+        // Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
+        // Bundle for Fragment to send parameter
+        Bundle args = new Bundle();
+        args.putInt("viewMode",getIntent().getIntExtra("viewMode",1));
+        args.putDouble("lat",getIntent().getDoubleExtra("lat",0));
+        args.putDouble("lon",getIntent().getDoubleExtra("lon",0));
+        fragment.setArguments(args);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.add(R.id.container, fragment);
+        // 最後にcommitを使用することで変更を反映します
+        transaction.commit();
     }
 
 }
---
+++ app/src/main/java/drgn/cafemap/GetAddressGeocoder.java
@@ -0,0 +1,64 @@
+package drgn.cafemap;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by musta on 2017/06/10.
+ */
+
+public class GetAddressGeocoder extends AsyncTask<String, String, String> {
+
+    private Context context;
+    private TextView addressTextView;
+    private double lat;
+    private double lon;
+
+    public GetAddressGeocoder(Context context, TextView addressTextView, double lat, double lon) {
+        this.context = context;
+        this.addressTextView = addressTextView;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // doInBackground前処理
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        Geocoder coder = new Geocoder(context);
+        String address = "";
+        try {
+            List<Address> addresses = coder.getFromLocation(lat, lon, 1);
+            address += addresses.get(0).getAddressLine(0) + addresses.get(0).getAddressLine(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return address;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        // doInBackground後処理
+        addressTextView.setText(result);
+    }
+}
